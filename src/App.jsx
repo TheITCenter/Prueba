6c3ac73a9b3dfbf281d566e4f28c0f297dc7ce96
Carlos Osorio <carlosoc@live.com>
2024-04-09 20:47:53 -0500

    Segundo commit

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.jsx</code> Primer prueba de modificacion
+          Edit <code>src/App.jsx</code> Segunda modificacion
         </p>
       </div>
       <p className="read-the-docs">
