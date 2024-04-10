cc0d051dd2332b8af84227990e297d575cc706d0
carlososo <46817846+carlososo@users.noreply.github.com>
2024-04-09 20:20:53 -0600

    Revert "Tercer commit - Ejemplo de un Pull Request"

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.jsx</code> Tercer modificacion
+          Edit <code>src/App.jsx</code> Segunda modificacion
         </p>
       </div>
       <p className="read-the-docs">
